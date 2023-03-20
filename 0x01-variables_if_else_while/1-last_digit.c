96f949992e4ddc9338e26048def423bce6fa38c5
hani61 <haniknight2003@gmail.com>
2023-03-19 17:09:41 -0700

    lslso

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,9 +3,9 @@
 #include <time.h>
 
 /**
- * main -> assign number to the variable n each time it is executed
- * and print the last digit of the number stored in the variable n
- * Return: Always 0 (success)
+ *main -> assign number to the variable n each time it is executed
+ *and print the last digit of the number stored in the variable n
+ *Return: Always 0 (success)
  */
 int main(void)
 {
